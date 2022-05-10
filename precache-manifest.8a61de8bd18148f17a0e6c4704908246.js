95cba05a5bfafcfbc42e6900cdfbe4875174abaf
Akshit Tomar <akshitt125@gmail.com>
2022-05-10 22:13:47 +0530

    Updates

---
+++ precache-manifest.8a61de8bd18148f17a0e6c4704908246.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdce3dbed831124317567c20621a5461",
+    "url": "/newsroom/index.html"
+  },
+  {
+    "revision": "2021d02bf6bc5d52a128",
+    "url": "/newsroom/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a2d0b489f92efa320b57",
+    "url": "/newsroom/static/js/2.94436294.chunk.js"
+  },
+  {
+    "revision": "9c19088db84563606874",
+    "url": "/newsroom/static/js/3.5526bd81.chunk.js"
+  },
+  {
+    "revision": "2021d02bf6bc5d52a128",
+    "url": "/newsroom/static/js/main.21a335f9.chunk.js"
+  },
+  {
+    "revision": "8168c8a08dc63db95e4a",
+    "url": "/newsroom/static/js/runtime~main.ea354c6a.js"
+  },
+  {
+    "revision": "8102d7dfd8afed8ebfd63d8cef889259",
+    "url": "/newsroom/static/media/loading.8102d7df.gif"
+  }
+]);
